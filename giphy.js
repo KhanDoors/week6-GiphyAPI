71c91f72afb767aa95d95285af47c0d41e60a2be
unknown <syyadkhan@yahoo.com>
2018-03-17 07:31:01 -0400

    mar170730

--- giphy.js
+++ giphy.js
@@ -1,50 +1,94 @@
 $( document ).ready(function() {
-
-
-  // Api Key: ohNxjejREZLWwWntXkyjW9nhbEAU4kuI
-
-var heros = ["batman", "superman", "ironman", "hulk", "thor", "wolverine", "spiderman", "flash", "wwoman", "cyborg"]
-
-  function displayGifButtons() {
-
-    // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search +
-    "app_id=ucfcodeschool";
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-
-      // Printing the entire object to console
-      console.log(response);
-
-      // Constructing HTML containing the artist information
-      var artistName = $("<h1>").text(response.name);
-      var artistURL = $("<a>").attr("href", response.url).append(artistName);
-      var artistImage = $("<img>").attr("src", response.thumb_url);
-      var trackerCount = $("<h2>").text(response.tracker_count + " fans tracking this artist");
-      var upcomingEvents = $("<h2>").text(response.upcoming_event_count + " upcoming events");
-      var goToArtist = $("<a>").attr("href", response.url).text("See Tour Dates");
-
-      // Empty the contents of the artist-div, append the new artist content
-      $("#artist-div").empty();
-      $("#artist-div").append(artistURL, artistImage, trackerCount, upcomingEvents, goToArtist);
-    });
+  // An array of actions, new actions will be pushed into this array;
+  var actions = ["Batman", "Ironman", "Wonder Woman", "Cyborg", "Captian America", "Flash", "Hulk", "Green Lantern", "Wolverine", "Supergirl","Superman", "Tarzan", "Aquaman"];
+  // Creating Functions & Methods
+  // Function that displays all gif buttons
+  function displayGifButtons(){
+      $("#gifButtonsView").empty(); // erasing anything in this div id so that it doesnt duplicate the results
+      for (var i = 0; i < actions.length; i++){
+          var gifButton = $("<button>");
+          gifButton.addClass("action");
+          gifButton.addClass("btn btn-primary")
+          gifButton.attr("data-name", actions[i]);
+          gifButton.text(actions[i]);
+          $("#gifButtonsView").append(gifButton);
+      }
   }
-
-  // Event handler for user clicking the select-artist button
-  $("#select-artist").on("click", function(event) {
-    // Preventing the button from trying to submit the form
-    event.preventDefault();
-    // Storing the artist name
-    var inputArtist = $("#artist-input").val().trim();
-
-    // Running the searchBandsInTown function (passing in the artist as an argument)
-    searchBandsInTown(inputArtist);
-  });
-
-
+  // Function to add a new action button
+  function addNewButton(){
+      $("#addGif").on("click", function(){
+      var action = $("#action-input").val().trim();
+      if (action == ""){
+        return false; // added so user cannot add a blank button
+      }
+      actions.push(action);
   
-});
-
+      displayGifButtons();
+      return false;
+      });
+  }
+  // Function to remove last action button
+      // Doesnt work properly yet removes all of the added buttons
+      // rather than just the last
+  function removeLastButton(){
+      $("removeGif").on("click", function(){
+      actions.pop(action);
+      displayGifButtons();
+      return false;
+      });
+  }
+  // Function that displays all of the gifs
+  function displayGifs(){
+      var action = $(this).attr("data-name");
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=ohNxjejREZLWwWntXkyjW9nhbEAU4kuI&limit=10";
+      console.log(queryURL); // displays the constructed url
+      $.ajax({
+          url: queryURL,
+          method: 'GET'
+      })
+      .done(function(response) {
+          console.log(response); // console test to make sure something returns
+          $("#gifsView").empty(); // erasing anything in this div id so that it doesnt keep any from the previous click
+          var results = response.data; //shows results of gifs
+          if (results == ""){
+            alert("There isn't a gif for this selected button");
+          }
+          for (var i=0; i<results.length; i++){
+  
+              var gifDiv = $("<div>"); //div for the gifs to go inside
+              gifDiv.addClass("gifDiv");
+              // pulling rating of gif
+              var gifRating = $("<p>").text("Rating: " + results[i].rating);
+              gifDiv.append(gifRating);
+              // pulling gif
+              var gifImage = $("<img>");
+              gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
+              gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
+              gifImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image
+              gifImage.attr("data-state", "still"); // set the image state
+              gifImage.addClass("image");
+              gifDiv.append(gifImage);
+              // pulling still image of gif
+              // adding div of gifs to gifsView div
+              $("#gifsView").prepend(gifDiv);
+          }
+      });
+  }
+  // Calling Functions & Methods
+  displayGifButtons(); // displays list of actions already created
+  addNewButton();
+  removeLastButton();
+  // Document Event Listeners
+  $(document).on("click", ".action", displayGifs);
+  $(document).on("click", ".image", function(){
+      var state = $(this).attr('data-state');
+      if ( state == 'still'){
+          $(this).attr('src', $(this).data('animate'));
+          $(this).attr('data-state', 'animate');
+      }else{
+          $(this).attr('src', $(this).data('still'));
+          $(this).attr('data-state', 'still');
+      }
+  });
+  });
+  
